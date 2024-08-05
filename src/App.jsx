04ab3fd6824cd1f3b291ae5dd7ba7901f838624e
Chotifz <chotife@gmail.com>
2024-08-05 20:07:10 +0700

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import Nav from "./components/Nav";
 import starImage from "./assets/star.png";
-import mainImage from "./assets/Rec3.png";
+
+import downloadImage from "./assets/download.png";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Mushroom Orange Lamp",
+      description: "Mushroom Orange Lamp desc",
+      price: 40,
+      imageUrl:
+        "https://res.cloudinary.com/dxiiecbza/image/upload/v1721356706/paa6_hfhwcw.jpg",
+    },
+  ];
   return (
-    <div className="bg-cover bg-no-repeat bg-center h-screen bg-rec">
-      <div className="max-w-7xl h-full px-8 py-12 mx-auto  sm:px-12 md:px-16 lg:px-24 sm:py-14 md:py-20  ">
-        <div className="flex flex-col justify-between rounded-[3rem] h-full px-6 py-2 bg-cyan-200">
-          <Nav />
-          <CardImage />
-          <Footer />
+    <div>
+      <div
+        className="absolute inset-0 bg-cover bg-no-repeat bg-center filter blur-md"
+        style={{ backgroundImage: `url(${products[0].imageUrl})` }}
+      ></div>
+      <div className=" relative bg-cover bg-no-repeat bg-center h-screen">
+        <div className="max-w-7xl h-full px-8 py-12 mx-auto  sm:px-12 md:px-16 lg:px-24 sm:py-14 md:py-20  ">
+          <div className="flex flex-col justify-between rounded-[3rem] h-full px-6 py-2 bg-cyan-200">
+            <Nav />
+            {products.map((product) => (
+              <CardImage key={product.id} {...product} />
+            ))}
+
+            <Footer />
+          </div>
         </div>
       </div>
     </div>
@@ -18,35 +38,43 @@ function App() {
 
 export default App;
 
-import React from "react";
-const CardImage = () => {
+const CardImage = ({ name, description, price, imageUrl }) => {
   return (
-    <div className="h-full w-full flex flex-col md:flex-row items-center justify-between gap-3">
-      <div className="h-20 mx-auto px-12  bg-black"></div>
+    <div className="h-full w-full flex flex-col sm:flex-row items-center justify-between max-sm:justify-start gap-2">
+      <div className="h-24 mx-auto px-12  bg-black"></div>
 
-      <div className="relative overflow-hidden rounded-[2rem] bg-center max-w-[45rem] h-full max-h-[32rem]">
+      <div className="relative bg-slate-100 overflow-hidden rounded-[2rem] bg-center max-w-[45rem] h-full max-h-[32rem] max-md:max-h-[26rem]">
         <img
           className="object-cover w-full h-full  "
-          src={mainImage}
+          src={imageUrl}
           alt="main-image"
         />
       </div>
 
-      <div className="h-11 mx-auto px-12  bg-black"></div>
+      <div className="hidden sm:block h-11 mx-auto px-12  bg-black"></div>
     </div>
   );
 };
+
 const Footer = () => {
   return (
-    <div className="w-full relative bg-cyan-50 h-20">
-      <div className="flex md:absolute bottom-10 w-full justify-between">
-        <div className="flex justify-between gap-3">
-          <div className=" w-24 mx-auto">
+    <div className="relative md:h-20 h-24">
+      <div className="flex absolute w-full bottom-4 max-sm:bottom-0 justify-between text-xs md:text-base">
+        <div className="flex justify-between gap-2">
+          <div className=" w-16 md:w-24 max-sm:w-14 mx-auto">
             <img src={starImage} alt="star" />
           </div>
-          <div className="text-xl flex-row  justify-between gap-2">
-            <div className="p-2 border">Satisfy Your Eyes</div>
-            <div className="p-2 border">Satisfy Your Eyes</div>
+          <div className=" flex-row  justify-between gap-2 ">
+            <div className="p-2 inline-block bg-cyan-200 rounded-r-3xl">
+              <h1 className="bg-white rounded-3xl  font-serif p-1  font-semibold">
+                Satisfy Your Eyes
+              </h1>
+            </div>
+            <div className="p-2 bg-cyan-200 rounded-r-3xl">
+              <h1 className="bg-white rounded-3xl font-serif p-1  font-semibold">
+                With Ocean Eyes App
+              </h1>
+            </div>
           </div>
         </div>
         <Download />
@@ -57,14 +85,31 @@ const Footer = () => {
 
 const Download = () => {
   return (
-    <div className=" flex-row">
-      <button className="bg-blue-500 py-1 px-5 rounded-lg text-lg">
-        Download
-      </button>
-      <span className="text-left">
-        <p>Number of downloads:</p>
-        <p className="text-xl font-bold"> 340M+</p>
-      </span>
+    <div className="bg-cyan-200 p-2 rounded-full relative ">
+      <div className=" bg-cyan-200 rounded-full bottom-5 right-2  px-2 border-4 border-yellow-100">
+        <div className=" flex">
+          <div className=" py-1 px-4">
+            <button className="text-xs text-blue-600 hover:text-red-500 font-semibold">
+              Download Now!
+            </button>
+            <span className="font-extrabold">
+              <p className="text-lg tracking-widest">100M+</p>
+              <p className="text-xs">USERS</p>
+            </span>
+          </div>
+
+          <button className=" h-20 w-20 relative bg-center pr-2 ">
+            <img
+              className="object-cover w-full  "
+              src={downloadImage}
+              alt="donwload"
+            />
+          </button>
+        </div>
+        <footer className="text-[10px] text-center font-semibold max-sm:hidden ">
+          Available On Platform :
+        </footer>
+      </div>
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <div className="flex items-center py-1 justify-between gap-4">
       <div className="w-[170px]">
-        <div className="flex w-40 bg-blue-300 hover:bg-purple-400 hover:border-2 text-center justify-center items-center h-10 rounded-md">
+        <div className="flex w-40 bg-blue-300 hover:bg-purple-400 hover:border text-center justify-center items-center h-10 rounded-md">
           <div className="cee"></div>
           <div className="bg-yellow-200 px-1">
             <button className="text-xs block font-mono rounded ">
@@ -14,10 +14,10 @@ const Nav = () => {
 
       <div className="md:flex-row flex flex-row-reverse items-center w-full">
         <div className="flex items-center justify-center bg-slate-50 gap-1 p-1.5 rounded-lg">
-          <button className="bg-yellow-200 hover:bg-purple-400 hover:border-2 rounded font-semibold text-xs py-1.5 px-1 md:px-3">
+          <button className="bg-yellow-200 hover:bg-purple-400 hover:border rounded font-semibold text-xs py-1.5 px-1 md:px-3">
             Sign up
           </button>
-          <button className="bg-blue-100 hover:bg-purple-400 hover:border-2 rounded py-1.5 px-1 md:px-3 max-[480px]:hidden text-xs font-extrabold font-serif">
+          <button className="bg-blue-100 hover:bg-purple-400 hover:border rounded py-1.5 px-1 md:px-3 max-[480px]:hidden text-xs font-extrabold font-serif">
             FREE TRIAL
           </button>
         </div>
