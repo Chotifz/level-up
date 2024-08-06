adf0cdda1362b298ab608d707decbfb2bf3a0848
Chotifz <chotife@gmail.com>
2024-08-06 16:48:51 +0700

    side image

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,88 @@ function App() {
     products.find((p) => p.category === "Ocean")
   );
   const [selectedProduct, setSelectedProduct] = useState(
-    selectedCategory.data[0]
+    selectedCategory.data[1]
   );
 
   const imageUrl = selectedProduct.imageUrl;
 
   return (
-    <div>
+    <div
+      className="bg-no-repeat bg-center filter bg-cover "
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
       <div
         className="absolute inset-0 bg-cover bg-no-repeat bg-center filter blur-md"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
+
       <div className=" relative bg-cover bg-no-repeat bg-center h-screen">
         <div className="max-w-7xl h-full px-8 py-12 mx-auto  sm:px-12 md:px-16 lg:px-24 sm:py-14 md:py-20  ">
           <div className="flex flex-col justify-between rounded-[3rem] h-full px-6 py-2 bg-cyan-200">
             <Nav />
+            <div className="w-full h-full overflow-hidden">
+              <div className="relative h-full w-full flex flex-col sm:flex-row items-center justify-between max-sm:justify-start gap-2">
+                {/*side image*/}
+                <div className="flex relative justify-center  h-24 w-full sm:h-64 mx-auto sm:w-26 sm:max-w-36 ">
+                  <div className="flex items-start justify-center flex-col gap-1  w-96  ">
+                    <div className="relative flex  justify-center items-center">
+                      <span className="p-2.5 rounded-full bg-yellow-200 "></span>
+                      <span className="p-[1px] px-1 bg-yellow-200 "></span>
+                      <h1 className="bg-white text-center px-2 py-0.5 rounded-2xl font-serif text-[13px]">
+                        Ocean Eyes
+                      </h1>
+                    </div>
 
-            <CardImage imageUrl={imageUrl} />
+                    {/* image container */}
+                    <div className=" flex sm:flex-col overflow-auto no-scrollbar gap-1">
+                      <ImageSide imageUrl={imageUrl} />
+                      <ImageSide imageUrl={imageUrl} />
+                      <ImageSide imageUrl={imageUrl} />
+                      <ImageSide imageUrl={imageUrl} />
+                    </div>
+                  </div>
+                </div>
+
+                <CardImage imageUrl={imageUrl} />
+
+                <div className="mx-auto px-2 flex max-sm:flex-row-reverse items-center text-[11px] tracking-wider font-mono font-semibold max-sm:gap-1">
+                  <div className="p-2 bg-yellow-200  rounded-2xl max-sm:hidden  ">
+                    <p className=" bg-cyan-200 textl  py-2 rounded-2xl ">
+                      SOSIAL MEDIA
+                    </p>
+                  </div>
+                  <div className="flex sm:flex-col gap-1 max-sm:bg-yellow-200 max-sm:p-0.5 max-sm:rounded-r-2xl max-sm:pr-2">
+                    <div className="p-1  bg-yellow-200 rounded-md">
+                      <div className="max-w-6 min-w-6 max-sm:max-w-5 max-sm:min-w-4 ">
+                        <a href="https://www.facebook.com">
+                          <img
+                            className="w-full"
+                            src={fbIcon}
+                            alt="facebook-icon"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="p-1  bg-yellow-200 rounded-md">
+                      <div className="max-w-6 min-w-6 max-sm:max-w-6 max-sm:min-w-5">
+                        <a href="https://www.instagram.com"></a>
+                      </div>
+                    </div>
+                    <div className="p-1  bg-yellow-200 rounded-md">
+                      <div className="max-w-6 min-w-6 max-sm:max-w-5 max-sm:min-w-4">
+                        <img src={xIcon} alt="x-icon" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-2 bg-yellow-200  sm:rounded-2xl max-sm:rounded-l-2xl">
+                    <p className=" bg-cyan-200 textr py-2.5 max-sm:py-0 max-sm:px-2 rounded-2xl tracking-widest">
+                      FIND US ON
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Footer />
+            </div>
           </div>
         </div>
       </div>
@@ -41,49 +106,12 @@ export default App;
 const CardImage = ({ imageUrl }) => {
   console.log(imageUrl);
   return (
-    <div className="w-full h-full">
-      <div className="h-full w-full flex flex-col sm:flex-row items-center justify-between max-sm:justify-start gap-2">
-        <div className="h-24 mx-auto px-12  bg-black"></div>
-
-        <div className="relative bg-slate-100 overflow-hidden rounded-[2rem] bg-center max-w-[45rem] h-full max-h-[32rem] max-md:max-h-[26rem]">
-          <img
-            className="object-cover w-full h-full  "
-            src={imageUrl}
-            alt="main-image"
-          />
-        </div>
-
-        <div className="mx-auto px-2 flex max-sm:flex-row-reverse items-center text-[11px] tracking-wider font-mono font-semibold max-sm:gap-1">
-          <div className="p-2 bg-yellow-200  rounded-2xl max-sm:hidden  ">
-            <p className=" bg-cyan-200 textl  py-2 rounded-2xl ">
-              SOSIAL MEDIA
-            </p>
-          </div>
-          <div className="flex sm:flex-col gap-1 max-sm:bg-yellow-200 max-sm:p-0.5 max-sm:rounded-r-2xl max-sm:pr-2">
-            <div className="p-1  bg-yellow-200 rounded-md">
-              <div className="max-w-6 min-w-6 max-sm:max-w-5 max-sm:min-w-4 ">
-                <img className="w-full" src={fbIcon} alt="facebook-icon" />
-              </div>
-            </div>
-            <div className="p-1  bg-yellow-200 rounded-md">
-              <div className="max-w-6 min-w-6 max-sm:max-w-6 max-sm:min-w-5">
-                <img src={instagramIcon} alt="instagram-icon" />
-              </div>
-            </div>
-            <div className="p-1  bg-yellow-200 rounded-md">
-              <div className="max-w-6 min-w-6 max-sm:max-w-5 max-sm:min-w-4">
-                <img src={xIcon} alt="x-icon" />
-              </div>
-            </div>
-          </div>
-          <div className="p-2 bg-yellow-200  sm:rounded-2xl max-sm:rounded-l-2xl">
-            <p className=" bg-cyan-200 textr py-2.5 max-sm:py-0 max-sm:px-2 rounded-2xl tracking-widest">
-              FIND US ON
-            </p>
-          </div>
-        </div>
-      </div>
-      <Footer />
+    <div className="relative bg-slate-100 overflow-hidden rounded-[2rem] bg-center max-w-[45rem] h-full max-h-[32rem] max-md:max-h-[26rem]">
+      <img
+        className="object-cover w-full h-full  "
+        src={imageUrl}
+        alt="main-image"
+      />
     </div>
   );
 };
@@ -146,6 +174,21 @@ const Download = () => {
         <footer className="text-[10px] text-center font-semibold max-sm:hidden">
           Available On Platform :
         </footer>
+      </div>
+    </div>
+  );
+};
+
+import React from "react";
+
+const ImageSide = ({ imageUrl }) => {
+  return (
+    <div className="relative flex  justify-center items-center">
+      <span className="p-2.5 rounded-full bg-yellow-200 "></span>
+      <span className="p-[1px] px-1 bg-yellow-200 "></span>
+      {/* g */}
+      <div className="bg-slate-100 p-1 rounded-xl relative sm:w-28 w-24 object-cover ">
+        <img className="rounded-xl" src={imageUrl} alt="" />
       </div>
     </div>
   );
